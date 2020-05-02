10cb4b9873f88fb2ad7665fc0c54ce4e7eec6b5d
Euller Boer <eullerboer2016@hotmail.com>
2020-05-02 14:43:36 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
@@ -41,5 +44,14 @@ public class Program {
 		seller.setName("Martha Wayne");
 		sellerdao.update(seller);
 		System.out.println("Update Completed");
+		
+		System.out.println("\n=== Teste6: seller Delete ===");
+		System.out.println("Digite um ID para deleção: ");
+		int id = sc.nextInt();
+		sellerdao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
+		
 	}
 }
